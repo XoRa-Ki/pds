5bef7725961c0d8817a527f40d8f77904359484a
MORTAIGNE Massimiliano <massimiliano.mortaigne@telecomnancy.eu>
2024-01-11 16:27:11 +0100

    add captain struct (first draft)

---
+++ src/structures/captain.c
@@ -0,0 +1,16 @@
+#include "captain.h"
+
+void whistle(marker place, )
+{
+
+}
+
+void toss()
+{
+
+}
+
+void cap_movement()
+{
+    
+}
---
+++ src/structures/captain.h
@@ -0,0 +1,18 @@
+#include "marker.h"
+
+#ifndef CAPTAIN_H
+#define CAPTAIN_H
+
+typedef struct captain
+{
+    int num_red_pik ;
+    int num_yel_pik ;
+    int num_blu_pik ;
+} ;
+
+void whistle(marker place, ) ;
+void toss() ;
+void cap_movement() ;
+
+
+#endif /*CAPTAIN_H*/
